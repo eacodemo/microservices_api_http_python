8c3e3aed2855e6e089145356d9e5d213cd8f294e
Deyner Morales <125691833+eacodemo@users.noreply.github.com>
2024-03-17 23:59:24 -0500

    update files (#6)
    * Cambios en las tablas BD
    
    * Se agrego logica y configuracion a estados (#2)
    
    Co-authored-by: eacodemo <eacodemo@gmail.com>
    
    * Feature/gestion ordenes (#3)
    
    * Se agrego endpoint y se configuro para ordenes
    
    * Se realizo cambios en tablas
    
    * Se agrego dependencia de mg
    
    ---------
    
    Co-authored-by: eacodemo <eacodemo@gmail.com>
    
    * Feature/gestion ordenes (#4)
    
    * Se agrego endpoint y se configuro para ordenes
    
    * Se realizo cambios en tablas
    
    * Se agrego dependencia de mg
    
    * Se reconfigura endpoints
    
    ---------
    
    Co-authored-by: eacodemo <eacodemo@gmail.com>
    
    * Cambio en tabla
    
    * Feature/gestion receta (#5)
    
    * Se agrego endpoints
    
    * Se agrego endpoints
    
    ---------
    
    Co-authored-by: eacodemo <eacodemo@gmail.com>
    
    ---------
    
    Co-authored-by: eacodemo <eacodemo@gmail.com>

--- gestion_estados/myapp.py
+++ gestion_estados/myapp.py
@@ -1,10 +1,76 @@
 from flask import Flask
+from flask.json import jsonify
+from models import Estado
+from settings import db, app
 
-app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '¡Bienvenido al microservicio del Restaurante!'
+
+
+
+# Ruta para obtener todos los estados
+@app.route('/estados', methods=['GET'])
+def get_all_estados():
+    """
+    Esta función está asignada con el punto final /estados y
+    representa todos los registros de estados utilizando el método GET HTTP.
+    """
+    message = {
+        'status': 404,
+        'message': 'Algo salió mal'
+    }
+    try:
+        # Recupera todos los registros de estados
+        data = Estado.query.all()
+        # Formatea los datos para devolverlos como JSON
+        estados = [{
+            'id': estado.id,
+            'estado': estado.estado
+        } for estado in data]
+        message.update({
+            'status': 200,
+            'message': 'Se recuperan TODOS los registros de estados',
+            'data': estados
+        })
+    except:
+        pass
+    return jsonify(message)
+
+# Otra ruta para obtener un estado específico por su ID
+@app.route('/estados/<int:estado_id>', methods=['GET'])
+def get_estado_by_id(estado_id):
+    """
+    Esta función está asignada con el punto final /estados/<id> y
+    representa un registro de estado específico utilizando el método GET HTTP.
+    """
+    message = {
+        'status': 404,
+        'message': 'Algo salió mal'
+    }
+    try:
+        # Recupera el estado por su ID
+        estado = Estado.query.get(estado_id)
+        if estado:
+            # Formatea el estado para devolverlo como JSON
+            estado_data = {
+                'id': estado.id,
+                'estado': estado.estado
+            }
+            message.update({
+                'status': 200,
+                'message': 'Se recupera el estado con el ID proporcionado',
+                'data': estado_data
+            })
+        else:
+            message.update({
+                'status': 404,
+                'message': 'No se encontró ningún estado con el ID proporcionado'
+            })
+    except:
+        pass
+    return jsonify(message)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- gestion_ordenes/myapp.py
+++ gestion_ordenes/myapp.py
@@ -1,10 +1,48 @@
 from flask import Flask
-
-app = Flask(__name__)
+from flask.json import jsonify
+from requests import request
+from settings import db, app
+from models import Orden
 
 @app.route('/')
 def index():
-    return '¡Bienvenido al microservicio del Restaurante!'
+    return '¡Bienvenido al microservicio de ordenes!'
+
+
+@app.route('/ordenes', methods=['GET'])
+def get_all_ordenes():
+    try:
+        ordenes = Orden.query.all()
+        ordenes_data = [{'id': orden.id, 'timestamp': orden.timestamp,
+                         'recetas_id': orden.recetas_id, 'estado_id': orden.estado_id} for orden in ordenes]
+        return jsonify({'status': 200, 'message': 'Se recuperan todas las órdenes', 'data': ordenes_data})
+    except Exception as e:
+        return jsonify({'status': 500, 'message': 'Error al recuperar órdenes', 'error': str(e)})
+
+@app.route('/ordenes', methods=['POST'])
+def create_orden():
+    try:
+        data = request.get_json()
+        nueva_orden = Orden(timestamp=data['timestamp'], recetas_id=data['recetas_id'], estado_id=data['estado_id'])
+        db.session.add(nueva_orden)
+        db.session.commit()
+        return jsonify({'status': 201, 'message': 'Orden creada exitosamente'})
+    except Exception as e:
+        return jsonify({'status': 500, 'message': 'Error al crear orden', 'error': str(e)})
+
+@app.route('/ordenes/<int:orden_id>', methods=['GET'])
+def get_orden_by_id(orden_id):
+    try:
+        orden = Orden.query.get(orden_id)
+        if orden:
+            orden_data = {'id': orden.id, 'timestamp': orden.timestamp,
+                          'recetas_id': orden.recetas_id, 'estado_id': orden.estado_id}
+            return jsonify({'status': 200, 'message': 'Orden encontrada', 'data': orden_data})
+        else:
+            return jsonify({'status': 404, 'message': 'Orden no encontrada'})
+    except Exception as e:
+        return jsonify({'status': 500, 'message': 'Error al recuperar orden', 'error': str(e)})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
