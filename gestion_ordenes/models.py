8d112a9f6798b7edbe7b823d36daaba9fd85e12e
Deyner Morales <125691833+eacodemo@users.noreply.github.com>
2024-03-17 23:38:09 -0500

    Feature/gestion ordenes (#3)
    * Se agrego endpoint y se configuro para ordenes
    
    * Se realizo cambios en tablas
    
    * Se agrego dependencia de mg
    
    ---------
    
    Co-authored-by: eacodemo <eacodemo@gmail.com>

--- gestion_ordenes/models.py
+++ gestion_ordenes/models.py
@@ -4,10 +4,8 @@ db = SQLAlchemy()
 
 
 class Orden(db.Model):
-    __tablename__ = 'Orden'
+    __tablename__ = 'Orden '  # Nombre de la tabla en la base de datos
     id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.TIMESTAMP, nullable=False)
+    timestamp = db.Column(db.Text, nullable=False)
     recetas_id = db.Column(db.Integer, db.ForeignKey('Recetas.Id'), nullable=False)
     estado_id = db.Column(db.Integer, db.ForeignKey('Estado.Id'), nullable=False)
-
-
